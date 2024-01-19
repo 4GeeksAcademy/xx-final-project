257ad13b816c165b29a92ce4a8ac47a44199b89a
Cammy Hale <cammyhale21@gmail.com>
2024-01-19 00:29:24 +0000

    Something

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 export const Login = () => {
 
     return(
         <div className="text-center mt-5">
             <input style={{margin: "5px"}} type="text" placeholder="email"/>
-            <input type="password" placeholder="password" />
+            <input type="password" className="pass" placeholder="password" />
+            <FontAwesomeIcon style={{margin: "5px"}} icon={faEye} />
             <button style={{margin: "5px"}}>Submit</button>
         </div>
     )
