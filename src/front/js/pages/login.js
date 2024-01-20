ac788450a9dbeec8d2e1cbd2241ab8d900e47fb9
Cammy Hale <cammyhale21@gmail.com>
2024-01-20 00:23:40 +0000

    password visibility functionality added

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+
+import "../../styles/login.css"
 
 export const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+    }
 
     return(
-        <div className="text-center mt-5">
-            <input style={{margin: "5px"}} type="text" placeholder="email"/>
-            <input type="password" className="pass" placeholder="password" />
-            <FontAwesomeIcon style={{margin: "5px"}} icon={faEye} />
-            <button style={{margin: "5px"}}>Submit</button>
+        <div className="login-body text-center mt-5">
+            <div className="inputs">
+                <input 
+                    className="email" 
+                    type="text" 
+                    placeholder="email"/>
+                <input 
+                    type={showPassword ? "text" : "password"} 
+                    className="pass" 
+                    placeholder="password" 
+                />
+            </div>
+            <div className="icons">
+                <FontAwesomeIcon 
+                    onClick={togglePassword} 
+                    className="eye-icon" 
+                    icon={showPassword ? faEyeSlash : faEye} />
+            </div>
+            <button className="submit-btn">Submit</button>
         </div>
     )
 }
