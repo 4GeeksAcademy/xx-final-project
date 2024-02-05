b6a844a29a4bc53a07713106022cdcd66f3a2043
Klaudine <109423321+klaudine@users.noreply.github.com>
2024-02-05 07:26:26 +0000

    updated login css

--- src/front/js/pages/ParkProfilePage.js
+++ src/front/js/pages/ParkProfilePage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
 import ParkProfileAccordion from '../component/ParkProfile/ParkProfileAccordion';
 import ParkProfileImage from '../component/ParkProfile/ParkProfileImage';
@@ -7,21 +7,41 @@ import '../../styles/parkProfilePage.css';
 import background from '../../img/login-background.jpg';
 import { useParams } from 'react-router-dom';
 
-export const ParkProfilePage = () => {
-    let theId = useParams().theid
-    const {store,actions}= useContext(Context)
-    let park = store.parkList.find((item)=>item.id==theId)
+export const ParkProfilePage =
+  () => {
+    let theId =
+      useParams().theid;
+    const { store, actions } =
+      useContext(Context);
+
+    // Find the park based on the ID
+    let park =
+      store.parkList.find(
+        (item) =>
+          item.id == theId
+      );
+
+    // Check if park is not defined before rendering components
+    if (!park) {
+      return (
+        <p>Park not found</p>
+      ); // You can render an error message or redirect to a different page
+    }
+
     return (
       <>
         <div
           className="background"
           style={{
-            backgroundImage: `url(${background})`
+            backgroundImage: `url(${background})`,
           }}
         >
           <div className="side-by-side-container">
-            <ParkProfileImage parkName={park.fullName}/>
-            <ParkProfileInfo park={park}/>
+            <ParkProfileImage />
+            {/* <ParkProfileImage parkName={park.fullName}/> */}
+            <ParkProfileInfo
+              park={park}
+            />
           </div>
           <ParkProfileAccordion />
         </div>
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,99 +1,201 @@
 import React, {
   useContext,
-    useEffect,
-    useState,
-  } from 'react';
-  import Button from 'react-bootstrap/Button';
-  import Form from 'react-bootstrap/Form';
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  import {
-    faEye,
-    faEyeSlash,
-  } from '@fortawesome/free-solid-svg-icons';
-  import { Context } from "../store/appContext"
+  useEffect,
+  useState,
+} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons';
+import { Context } from '../store/appContext';
 
-import Background from "../../img/login-background.jpg"
+import Background from '../../img/login-background.jpg';
 
-import "../../styles/login.css"
+import '../../styles/login.css';
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-    const {store, actions} = useContext(Context);
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [showPassword, setShowPassword] = useState(false);
-    const navigate = useNavigate();
+  const { store, actions } =
+    useContext(Context);
+  const [email, setEmail] =
+    useState('');
+  const [
+    password,
+    setPassword,
+  ] = useState('');
+  const [
+    showPassword,
+    setShowPassword,
+  ] = useState(false);
+  const navigate =
+    useNavigate();
 
-    const token = sessionStorage.getItem("token")
-    const togglePassword = () => {
-        setShowPassword(!showPassword);
-    }
-
-    const handleClick = (e) => {
-      e.preventDefault();
-      actions.login(email,password); 
-      navigate ("/")
-    }
-
-    useEffect(() => {
-      if(store.token && store.token != "" && store.token != undefined) navigate("/")
-    })
-
-    return(
-        <div className='background' style={{ backgroundImage: `url(${Background})`, width: "100%", height: "100vh"}}>
-          <Form.Label className='login-title' style={{fontSize: "50px", display: "flex", justifyContent: "center"}}>Log In Form</Form.Label>
-            {store.token && store.token != "" && store.token != undefined ? (
-              "You have successfully logged in" + store.token
-            ) : (
-              <div>
-                <Form onSubmit={handleClick}> 
-                  <Form.Group className="mt-1" controlId="formBasicEmail">
-                    <Form.Label> Email address </Form.Label>
-                    <Form.Control 
-                      type="email" 
-                      placeholder="Enter email" 
-                      value={email} 
-                      onChange={e => setEmail(e.target.value)}
-                    />
-                  </Form.Group>
-
-                  <Form.Group className="mt-3" controlId="formBasicPassword" >
-                    <Form.Label> Password </Form.Label>
-                    <div style={{ position: 'relative'}} >
-                      <Form.Control 
-                        type={ showPassword ? 'text' : 'password'}
-                        placeholder="Enter password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                      />
-                      <FontAwesomeIcon className='eye' style={{ position:'absolute' }} onClick={ togglePassword }
-                        icon={
-                          showPassword
-                            ? faEyeSlash
-                            : faEye
-                        }
-                      />
-                    </div>
-                      <Form.Text className="text-muted">
-                        We'll never share
-                        your password with
-                        anyone else.
-                      </Form.Text>
-                  </Form.Group>
-                    <Button
-                      className="mt-3 submit-btn"
-                      variant="primary"
-                      type="submit"
-                    >
-                      Login
-                    </Button>
-                    <Button
-                      className="mt-3 submit-btn"
-                      variant="primary"
-                    >Forgot Password?</Button>
-          </Form>
-              </div>
-            )}
-        </div>
+  const token =
+    sessionStorage.getItem(
+      'token'
+    );
+  const togglePassword =
+    () => {
+      setShowPassword(
+        !showPassword
       );
     };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    actions.login(
+      email,
+      password
+    );
+    navigate('/');
+  };
+
+  useEffect(() => {
+    if (
+      store.token &&
+      store.token != '' &&
+      store.token != undefined
+    )
+      navigate('/');
+  });
+
+  return (
+    <div
+      className="background"
+      style={{
+        backgroundImage: `url(${Background})`,
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <Form.Label
+        className="login-title"
+        style={{
+          fontSize: '50px',
+          display: 'flex',
+          justifyContent:
+            'center',
+        }}
+      ></Form.Label>
+      {store.token &&
+      store.token != '' &&
+      store.token !=
+        undefined ? (
+        'You have successfully logged in' +
+        store.token
+      ) : (
+        <div className="formContainer">
+          <Form
+            onSubmit={
+              handleClick
+            }
+          >
+            <h4>Log in</h4>
+            <h6>
+              Welcome to SIGHT
+              SEEKER
+            </h6>
+            <Form.Group
+              className="mt-1"
+              controlId="formBasicEmail"
+            >
+              <Form.Label>
+                {' '}
+                Email{' '}
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(
+                  e
+                ) =>
+                  setEmail(
+                    e.target
+                      .value
+                  )
+                }
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mt-3"
+              controlId="formBasicPassword"
+            >
+              <Form.Label>
+                {' '}
+                Password{' '}
+              </Form.Label>
+              <div
+                style={{
+                  position:
+                    'relative',
+                }}
+              >
+                <Form.Control
+                  type={
+                    showPassword
+                      ? 'text'
+                      : 'password'
+                  }
+                  placeholder="Enter password"
+                  value={
+                    password
+                  }
+                  onChange={(
+                    e
+                  ) =>
+                    setPassword(
+                      e.target
+                        .value
+                    )
+                  }
+                />
+                <FontAwesomeIcon
+                  className="eye"
+                  style={{
+                    position:
+                      'absolute',
+                  }}
+                  onClick={
+                    togglePassword
+                  }
+                  icon={
+                    showPassword
+                      ? faEyeSlash
+                      : faEye
+                  }
+                />
+              </div>
+              <Form.Text className="text-muted">
+                We'll never
+                share your
+                password with
+                anyone else.
+              </Form.Text>
+            </Form.Group>
+            <div className="buttonContainer">
+              <Button
+                className="mt-3 submit-btn"
+                // variant="primary"
+                type="submit"
+              >
+                Login
+              </Button>
+              <Button
+                className="mt-3 submit-btn"
+                // variant="primary"
+              >
+                Forgot
+                Password?
+              </Button>
+            </div>
+          </Form>
+        </div>
+      )}
+    </div>
+  );
+};
