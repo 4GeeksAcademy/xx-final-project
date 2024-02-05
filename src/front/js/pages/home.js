f76a20605e00fb30f9760b6dc1035ab5f85bcaba
Klaudine <109423321+klaudine@users.noreply.github.com>
2024-02-05 08:05:13 +0000

    set changes to home.js cards

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Card, Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
@@ -7,22 +9,36 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
+    <div className="homeCardContainer ">
     <div className="text-center mt-5">
-      <div>
-        <h1>Parks</h1>
-        <div className="">
-          {store.parkList?.map((park, index) => {
-            return (
-              <div className="">
-                {park.fullName}
+      <h1>Parks</h1>
+      {/* <div >
+        {store.parkList?.map((park, index) => (
+          <Card key={index} className="m-2">
+            <Card.Body>
+              <Card.Title>{park.fullName}</Card.Title>
+              <Link to={"/parkprofilepage/" + park.id}>
+                <Button variant="primary">Learn More</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </div> */}
+      <Row>
+        {store.parkList?.map((park, index) => (
+          <Col key={index} lg={4}>
+            <Card className="m-5">
+              <Card.Body>
+                <Card.Title className="mx-4">{park.fullName}</Card.Title>
                 <Link to={"/parkprofilepage/" + park.id}>
-                  <button className="btn btn-primary">Learn More</button>
+                  <Button className="mt-4" variant="primary">Learn More</Button>
                 </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
     </div>
   );
 };
